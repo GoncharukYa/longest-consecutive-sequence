655e756cde4e4aafc4cfebc4d746e6cbb58ce601
Goncharuk Yaroslav <gonyarik@gmail.com>
2018-03-15 17:37:47 +0300

    600ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length == 0) { 
+    return 0;
+  };
+  if (array.length == 1) { 
+    return 1;
+  };
+  let set = new Set(array);
+  let currentLength =  1;
+  let maxLength = 1;
+  let currentNum;
+  for (let key of set) { 
+    if (!set.has(key - 1)) {
+      currentNum = key;
+      currentLength = 1;
+    };
+    while (set.has(currentNum + 1)) { 
+      currentLength++;
+      currentNum++;
+      if (maxLength < currentLength) { 
+        maxLength = currentLength;
+      };
+    };
+  };
+  return maxLength;
 }
